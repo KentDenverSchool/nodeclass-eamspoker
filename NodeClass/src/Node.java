a741d794fa21b563f2faeb6b39effe5e6962e679
Emily Amspoker <eamspoker21@kentdenver.org>
2019-09-06 17:33:05 -0600

    added constructors

--- NodeClass/src/Node.java
+++ NodeClass/src/Node.java
@@ -1,6 +1,45 @@
 public class Node<T> {
     //T as in type
-    private T nodeValue;
-    private T after;
-    private T before;
+    private T data;
+    private Node front;
+    private Node back;
+
+    //empty constructor, all instance data are null
+    public Node(){
+    }
+
+    //data is intialized, back and front are null (as in a one-item ArrayList)
+    public Node(T data){
+        this.data = data;
+    }
+
+
+    //data is initialized along with either front or back (end or beginning of ArrayList)
+    public Node(T data, Node node, boolean isFront){
+        this.data = data;
+        //if it is the front item, there are only items behind it
+        if(isFront){
+            this.back = node;
+        } else {
+            this.front = node;
+        }
+    }
+
+    //initialize all instance data
+    public Node(T data, Node front, Node back){
+        this.data = data;
+        this.back = back;
+        this.front = front;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
